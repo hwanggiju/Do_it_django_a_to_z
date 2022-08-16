40df71f7f413b3281309e56e2932a6889bbac51b
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-08-16 11:30:38 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,20 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(
+            username='trump',
+            password='somepassword'
+        )
+        self.user_obama = User.objects.create_user(
+            username='obama',
+            password='somepassword'
+        )
     def navbar_test(self, soup):
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
@@ -32,16 +41,55 @@ class TestView(TestCase):
 
         self.navbar_test(soup)
 
+        self.assertEqual(Post.objects.count(), 0)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
+
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world',
+            author = self.user_trump
+        )
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요?',
+            author=self.user_obama
+        )
+
+        self.assertEqual(Post.objects.count(), 2)
+
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual(response.status_code, 200)
+        main_area = soup.find('div', id='main-area')
+
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+
+        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
+
     def test_post_detail(self):
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world',
+            author = self.user_trump,
         )
 
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
-
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(post_001.content, post_area.text)
 
         self.navbar_test(soup)
